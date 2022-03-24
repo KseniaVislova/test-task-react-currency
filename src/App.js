f777f138760e9bdb0ce34f77fea9b01c44615d7b
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-03-24 19:32:14 +0300

    feat: add getting of last 10 days data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import s from './App.module.css';
 function App() {
   const [url, setUrl] = useState('https://www.cbr-xml-daily.ru/daily_json.js');
   const [previousURL, setPreviousURL] = useState('');
+  const [lastDaysAll, setLastDaysAll] = useState([]);
+  const [lastDays, setLastDays] = useState([]);
+  const [current, setCurrent] = useState('');
   const [valute, setValute] = useState([]);
 
   const getData = async() => {
@@ -13,7 +16,6 @@ function App() {
       setPreviousURL(res.PreviousURL);
       let arr = [];
       for (let key in res.Valute) {
-        console.log(res.Valute[key])
         arr.push(res.Valute[key])
       }
       setValute(arr);
@@ -22,18 +24,73 @@ function App() {
     }
   }
 
+  const getLastDaysAll = async(currency) => {
+    console.log(lastDays);
+    let arr = [];
+    let url = previousURL;
+    for (let i = 0; i < 10; i++) {
+      try {
+        let res = await fetch(url);
+        res = await res.json();
+        console.log(res.Valute);
+        url = res.PreviousURL;
+        const obj = {date: res.Date, valute: res.Valute};
+        arr.push(obj);
+      } catch (err) {
+        console.error(err);   
+      }
+    }
+    setLastDaysAll(arr);
+    setCurrent(currency)
+  }
+
+  const getLastDays = () => {
+    const arr = lastDaysAll.map(item => {
+      let obj = {};
+      for (let key in item) {
+        if (key === 'date') {
+          obj[key] = item[key]
+        }
+        if (key === 'valute') {
+          for (let i in item[key]) {
+            if (i === current) {
+              obj[key] = item[key][i];
+            }
+          }
+        }
+      }
+      return obj;
+    })
+    console.log(arr);
+    setLastDays(arr);
+  }
+
   useEffect(() => {
     getData();
   }, [url])
+
+  useEffect(() => {
+    getLastDays()
+  }, [lastDaysAll])
+
+  useEffect(() => {
+    console.log(lastDays)
+  }, [lastDays])
 
   return (
     <div>
       <h1>Valute APP</h1>
       <ul>
         {valute.map(item => 
-          <li key={item.ID} className={s.item}>
+          <li key={item.ID} className={s.item} onClick={() => getLastDaysAll(item.CharCode)}>
             <span>{item.CharCode} {item.Value} {((item.Value - item.Previous) / item.Previous) * 100}%</span>
             <span className={s.tooltip}>{item.Name}</span>
+            {current === item.CharCode 
+            ? <ul>
+              {lastDays.map(item => 
+                <li>{item.date} <span>{item.valute.Value} {((item.valute.Value - item.valute.Previous) / item.valute.Previous) * 100}%</span></li>
+              )}
+            </ul> : ''}
           </li>)}
       </ul>
     </div>
